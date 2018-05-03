ea1dfefea2203bcb34a8b64c6166385ad91bcc1c
herbherbherb <rwang67@LAPTOP-SAD2BRRE.localdomain>
2018-05-03 18:26:54 -0500

    5/3

---
+++ CommentSentiment/driver.py
@@ -0,0 +1,17 @@
+import comment_extract as CE
+import sentimentYouTube as SYT
+
+def main():
+	# EXAMPLE VideoId = 'tCXGJQYZ9JA'
+	videoId = input("Enter VideoId : ")
+	# videoId = 'tCXGJQYZ9JA'
+	# Fetch the number of comments
+	# if count = -1, fetch all comments
+	count = int(input("Enter no. of comments to extract : "))
+	comments = CE.commentExtract(videoId, count)
+	print(comments)
+	SYT.sentiment(comments)
+
+
+if __name__ == '__main__':
+	main()
